ddcb83fa37adf5b59e898e2bad3de696860f0172
Berat Azgun <brtazgun@gmail.com>
2023-06-25 12:13:27 +0300

    followersSchema added

--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -3,10 +3,12 @@ import { usersSchema } from './users.schema'
 import { postsSchema } from './posts.schema'
 import { commentsSchema } from './comments.schema'
 import { likesSchema } from './likes.schema'
+import { followersSchema } from './followers.schema'
 
 export const typeDefs = mergeTypeDefs([
 	usersSchema,
 	postsSchema,
 	commentsSchema,
 	likesSchema,
+	followersSchema,
 ])

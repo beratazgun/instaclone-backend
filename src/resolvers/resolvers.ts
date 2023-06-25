6aaddce6cd5034c2c4fe845d5f716c9ce9a23bcf
Berat Azgun <brtazgun@gmail.com>
2023-06-25 12:13:14 +0300

    followersResolver added

--- src/resolvers/resolvers.ts
+++ src/resolvers/resolvers.ts
@@ -3,10 +3,12 @@ import { usersResolver } from './users.resolver'
 import { postsResolver } from './posts.resolver'
 import { commentsResolver } from './comments.resolver'
 import { likesResolver } from './likes.resolver'
+import { followersResolver } from './followers.resolver'
 
 export const resolvers = mergeResolvers([
 	usersResolver,
 	postsResolver,
 	commentsResolver,
 	likesResolver,
+	followersResolver,
 ])
